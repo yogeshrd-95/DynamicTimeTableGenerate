0969d712cabdc9417a8abc797d1e679410fead9e
yogeshrd-95 <yrajput794@gmail.com>
2023-09-21 09:36:10 +0530

    Add project files.

---
+++ DynamicTimeTableGenerate/Controllers/GenerateTimeTableController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicTimeTableGenerate.Controllers
+{
+    public class GenerateTimeTableController : Controller
+    {
+        // GET: GenerateTimeTable
+
+        public ActionResult Index(int? intTotalSubjects, int? intTotalHoursForWeek)
+        {
+            ViewBag.TotalSubjects = intTotalSubjects;
+
+            ViewBag.TotalHoursForWeek = intTotalHoursForWeek;
+            return View();
+        }
+    }
+}
---
+++ DynamicTimeTableGenerate/Controllers/TimeTableController.cs
@@ -0,0 +1,35 @@
+﻿using DynamicTimeTableGenerate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicTimeTableGenerate.Controllers
+{
+    public class TimeTableController : Controller
+    {
+        // GET: TimeTable
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(WorkingDays workingDays)
+        {
+            int intTotalHoursForWeek = 0, intTotalSubjects = 0;
+            TempData["WorkingDays"] = workingDays.NoOfWorkingDays;
+            TempData["SubjectsPerDay"] = workingDays.NoOfSubjectsPerDay;
+            intTotalSubjects = workingDays.TotalSubjects;
+            intTotalHoursForWeek = workingDays.NoOfWorkingDays * workingDays.NoOfSubjectsPerDay;
+            return RedirectToAction("Index", "GenerateTimeTable", new
+            {
+                intTotalSubjects,
+                intTotalHoursForWeek,
+            });
+
+        }
+    }
+}
